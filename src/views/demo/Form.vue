d49fc9999e89533b490c59c932d5a6b8cfb0ed33
刘中泉 <liuzhongquan@96900.com.cn>
2017-09-21 20:29:23 +0800

    update some component

--- src/views/demo/Form.vue
+++ src/views/demo/Form.vue
@@ -4,37 +4,38 @@
 			表单元素
 		</div>
 		<ev-input name="name" v-model="title" required :maxlength="100">标题</ev-input>
-		<div>
-			我已经
-		<ev-count-star ref='countstart' class='example' :start-val='count.start' :end-val='1992' :duration='count.duration' :decimals='_decimals' 
-		:separator='count.separator' prefix='从' :suffix='count.suffix' :autoplay='false'></ev-count-star>
-		<ev-count-star ref='countend' class='example' :start-val='count.start' :end-val='count.end' :duration='count.duration' :decimals='_decimals' 
-		:separator='count.separator' prefix='到' :suffix='count.suffix' :autoplay='false'></ev-count-star>
-		走过了25载。
+		<div class="example-box">
+			从1992到
+		<ev-count-star ref='countstart' class='example' :start-val='count.start' :end-val='count.end' :duration='count.duration' 
+		separator='' prefix='' suffix='' :autoplay='true'></ev-count-star>走过了
+		<ev-count-star ref='countend' class='example' :start-val='0' :end-val='count.age' :duration='count.duration' 
+		separator='' prefix='' suffix='' :autoplay='true'></ev-count-star>载
+		</div>
 		<el-button type="primary" size="mini" @click="countPlay">播放</el-button>
 		<el-button type="primary" size="mini" @click="countPause">暂停</el-button>
-		</div>
-		
+		<ev-sticky className="sub-navbar" :stickyTop="80" :zIndex="999">
+			<div>滚动页面我会吸附在顶端</div>
+		</ev-sticky>
+		<div style="height:900px;background-color:#13ce66;"></div>
 	</div>
 </template>
 
 <script>
 import EvCountStar from "components/EvCountStar";
 import EvInput from "components/EvInput";
+import EvSticky from 'components/EvSticky';
 	export default {
 		components:{
-			EvCountStar,EvInput
+			EvCountStar,EvInput,EvSticky
 		},
 		data: function(){
 			return {
 				title:'',
 				count:{
-					start: 0,
+					start: 1992,
 					end: 2017,
 					duration: 4000,
-					setDecimals: 0,
-					separator: ',',
-					suffix: ' 年',
+					age:0
 				}
 			}
 		},
@@ -51,9 +52,23 @@ import EvInput from "components/EvInput";
 			}
 		},
 		beforeMount(){
+			this.count.end=~~new Date().format('yyyy');
+			this.count.age=this.count.end-this.count.start;
 		}
 	}
 </script>
 
 <style scoped>
+.example-box{
+	margin: 10px 0;
+	font-size:30px;
+	line-height:80px;
+}
+.example {
+	font-size: 80px;
+	color: #F6416C;
+	display: inline-block;
+	text-align: center;
+	font-weight: 500;
+}
 </style>
